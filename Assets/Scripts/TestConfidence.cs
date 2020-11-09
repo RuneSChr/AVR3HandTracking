ad5a5e96f1fe4b2f09a76de20d2d12497523eafc
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2020-11-09 20:11:26 +0100

    avr quest tracking stuff

---
+++ Assets/Scripts/TestConfidence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class TestConfidence : MonoBehaviour
+{
+
+    [SerializeField]
+    private Material Confidence, instance;
+    public OVRHand hand;
+    private Color red, green;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = Confidence;
+        red = Color.red;
+        green = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeFingerMatColor(OVRHand.TrackingConfidence confidence)
+    {
+        if (confidence == OVRHand.TrackingConfidence.Low)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color= Color.green;
+        }
+
+    }
+
+    
+}
