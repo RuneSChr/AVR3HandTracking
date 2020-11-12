0b8e9cbfdf5eb4f8018ec7b08d7639a2951efd12
RuneSChr <runechr2014@gmail.com>
2020-11-12 11:10:37 +0100

    gesture recognize

---
+++ Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityScript.Scripting.Pipeline;
+
+
+//class without function
+[System.Serializable]
+public struct Gesture
+{
+    public string name;
+    public List<Vector3> fingerPositions;
+    public UnityEvent onRecognized;
+}
+
+public class GestureDetector : MonoBehaviour
+{
+    public OVRSkeleton skeleton;
+    public List<Gesture> savedGestures;
+    public float threshold = 0.1f;
+
+    public bool debugMode;
+
+    private Gesture previousGes;
+    private void Start()
+    {
+        previousGes = new Gesture();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugMode && Input.GetKeyDown(KeyCode.Space))
+        {
+            Save();
+        }
+
+        Gesture currentGes = Recognize();
+        bool hasRecognized = !currentGes.Equals(new Gesture());
+        if (hasRecognized && !currentGes.Equals(previousGes))
+        {
+            Debug.Log("Gesture recognized: " + currentGes.name);
+            previousGes = currentGes;
+            currentGes.onRecognized.Invoke();
+        }
+    }
+
+    public void Save()
+    {
+        Gesture g = new Gesture();
+        g.name = "New gesture";
+        List<Vector3> data = new List<Vector3>();
+        foreach (var bone in skeleton.Bones)
+        {
+            //compare 
+            data.Add(skeleton.transform.InverseTransformPoint(bone.Transform.position));
+        }
+
+        g.fingerPositions = data;
+        savedGestures.Add(g);
+    }
+
+    Gesture Recognize()
+    {
+        Gesture ges = new Gesture();
+        float minDif = Mathf.Infinity;
+
+        foreach (var gesture in savedGestures)
+        {
+            float distanceSum = 0;
+            bool discarded = false;
+
+            for (int i = 0; i < skeleton.Bones.Count; i++)
+            {
+                Vector3 currPosition = skeleton.transform.InverseTransformPoint(skeleton.Bones[i].Transform.position);
+                float distance = Vector3.Distance(currPosition, gesture.fingerPositions[i]);
+                if (distance > threshold)
+                {
+                    discarded = true;
+                    break;
+                }
+                distanceSum += distance;
+            }
+            if (!discarded && distanceSum < minDif)
+            {
+                minDif = distanceSum;
+                ges = gesture;
+            }
+        }
+
+        return ges;
+    }
+}
