446a6908cf64fcb35cd4ff0360bdc8c66ce7c3e2
Smitchy <47454064+Smitchy@users.noreply.github.com>
2020-11-12 11:41:45 +0100

    Added weapons. Rawr.

---
+++ Assets/Scripts/UnityWeapons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class UnityWeapons : iWeapon
+{
+    [SerializeField]
+    private ParticleSystem[] partSystems;
+
+    [SerializeField]
+    private bool testBegin, testEnd;
+    public override void BeginShoot()
+    {
+        foreach(ParticleSystem ps in partSystems)
+        {
+            ps.Play();
+        }
+    }
+
+    public override void EndShoot()
+    {
+        foreach (ParticleSystem ps in partSystems)
+        {
+            ps.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+        }
+    }
+    private void Update()
+    {
+        if (testBegin)
+        {
+            BeginShoot();
+            testBegin = false;
+        }
+        if (testEnd)
+        {
+            EndShoot();
+            testEnd = false;
+        }
+    }
+}
---
+++ Assets/Scripts/iWeapon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class iWeapon : MonoBehaviour
+{
+    public abstract void BeginShoot();
+    public abstract void EndShoot();
+}
