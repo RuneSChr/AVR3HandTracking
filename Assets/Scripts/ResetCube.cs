829f726802bd43f29b2aadeb0577a0ec7321d864
RuneSChr <runechr2014@gmail.com>
2020-11-13 10:24:12 +0100

    updated pyramid

---
+++ Assets/Scripts/ResetCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResetCube : MonoBehaviour
+{
+    private Vector3 position;
+    private Rigidbody rb;
+    bool running;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        position = gameObject.transform.position;
+    }
+
+    private void Awake()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !running)
+        {
+            //running = true;
+            rb.velocity = Vector3.zero;
+            gameObject.transform.position = position;
+        }
+    }
+}
